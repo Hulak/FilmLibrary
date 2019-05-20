096671859b90f10f67aba38aef15041a76c40df5
Alyona <alena.hulak@gmail.com>
2019-05-20 11:29:27 +0300

    Add lab1

---
+++ lab1/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFirst
+{
+    public class Rectangle
+    {
+        public double X, Y;
+        public Rectangle()
+        {
+            X = 1;
+            Y = 1;
+        }
+        public Rectangle(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double Square()
+        {
+            return X * Y;
+        }
+    }
+}
